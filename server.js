43d81ff302003e6a023db747d21d9857d0f70fbd
rahulelnino <rahulliers@gmail.com>
2018-11-11 13:49:16 +0530

    Add files via upload
    new branch

--- server.js
+++ server.js
@@ -1,37 +1,50 @@
-
+const express = require('express');
 const http = require('http');
-const events = require('events');
-const eventEmitter = events.EventEmitter;
-const emitter = new eventEmitter();
-const fs = require('fs') ;
+const bodyparser = require('body-parser');
+const cors = require('cors');
+// const events = require('events');
+// const eventEmitter = events.EventEmitter;
+// const emitter = new eventEmitter();
+// const fs = require('fs') ;
+const port = 3000;
+const app = express();
+app.use(bodyparser.json());
+app.use(cors());
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,({'Content-Type':'application/json'}));
-    res.end('Hello AB');
-    console.log(req.method);
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200,({'Content-Type':'application/json'}));
+//     res.end('Hello AB');
+//     console.log(req.method);
+//    });
+   app.get('/',(req,res)=>{
+       res.send("Hello from Server");
    });
-   
-   server.listen(3010,()=>{
-       console.log("Server listening to port 3010");
+
+   app.post('/enroll',(req,res)=>{
+       console.log(req.body);
+       res.status(401).send({"message":"Data Received"});
+   })
+   app.listen(port,()=>{
+       console.log("Server listening to port "+port);
     //    console.log(__filename);
     //    console.log(__dirname);
     //    console.log(process.cwd())
     //    console.log(process.env.my_current_env)
    });
-var dater="Hi, This is Ian Bell";
+// var dater="Hi, This is Ian Bell";
 
-   fs.readFile('read.txt','utf-8',(err,data)=>{
-      if(err) {
-          console.log(err);
-      }
-      console.log('Data is successfully read'+Date.now());
-    });
-   fs.writeFile(__dirname +'/writre.txt',dater,(err,data)=>{
-          if(err){
-              console.log(err);
-          }
-          console.log('Data is successfully written'+Date.now())
-      });
+//    fs.readFile('read.txt','utf-8',(err,data)=>{
+//       if(err) {
+//           console.log(err);
+//       }
+//       console.log('Data is successfully read'+Date.now());
+//     });
+//    fs.writeFile(__dirname +'/writre.txt',dater,(err,data)=>{
+//           if(err){
+//               console.log(err);
+//           }
+//           console.log('Data is successfully written'+Date.now())
+//       });
 
 
 // emitter.cricket= ()=>{
